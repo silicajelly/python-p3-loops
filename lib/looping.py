4e7b9caadbb373736cde80c2a130ce718cabdb4a
melvinjames <melvin.gacheru@student.morigaschool.com>
2023-05-22 09:23:02 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,50 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
-
-def square_integers(int_list):
-    # code goes here!
-    pass
-
+def admin_login(username, password):
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+result = admin_login("ADMIN", "12345")
+print(result)
+def hows_the_weather(temperature):
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+temperature = 66
+result = hows_the_weather(temperature)
+print(result)
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+fizzbuzz()
+def calculator(operation, num1, num2):
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Error: Division by zero is not allowed!")
+            return None
+    else:
+        print("Invalid operation!")
+        return None
+result = calculator('+', 5, 3)
+print(result)  # Output: 8
